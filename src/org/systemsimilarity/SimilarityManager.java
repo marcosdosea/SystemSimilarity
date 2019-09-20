bf81488b455c2d80e609fcd6bf35c4a5a5308b28
marcosdosea <marcosdosea@yahoo.com.br>
2019-09-19 23:29:42 -0300

    mudança da interface para retorno do resultado.

--- src/org/systemsimilarity/SimilarityManager.java
+++ src/org/systemsimilarity/SimilarityManager.java
@@ -1,69 +1,92 @@
 package org.systemsimilarity;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import org.designroleminer.ClassMetricResult;
 import org.designroleminer.threshold.DesignRoleTechnique;
+import org.designroleminer.threshold.TechniqueExecutor;
 
 public class SimilarityManager {
 
-	public float calculate(ArrayList<ClassMetricResult> projeto1, ArrayList<ClassMetricResult> projeto2) {
+	
+	
+	public ArrayList<String> filterSimilarProjects(String project, List<SimilarityResult> listSimilarity, double threshold) {
+		ArrayList<String> filteredProjects = new ArrayList<String>();
+		for(SimilarityResult result: listSimilarity) {
+			if (result.getProject1().equals(project)) {
+				if (result.getSimilarity() >= threshold) {
+					filteredProjects.add(result.getProject2());
+				}
+			}
+		}
+		return filteredProjects;
+	}
+	
+	public List<SimilarityResult> calculate(ArrayList<String> projetosReferencia, String pastaMetricas) {
+
+		Map<String, ArrayList<ClassMetricResult>> mapProjetosMetricas = new HashMap<String, ArrayList<ClassMetricResult>>();
+		TechniqueExecutor executor = new TechniqueExecutor(new DesignRoleTechnique());
+
+		ArrayList<String> listaUnicoProjeto;
+		for (String projeto : projetosReferencia) {
+			listaUnicoProjeto = new ArrayList<String>();
+			listaUnicoProjeto.add(projeto);
+			mapProjetosMetricas.put(projeto, executor.getMetricsFromProjects(listaUnicoProjeto, pastaMetricas));
+		}
+
+		List<SimilarityResult> listSimilarityResult = new ArrayList<SimilarityResult>();
+		Iterator<String> it1 = mapProjetosMetricas.keySet().iterator();
+		Iterator<String> it2;
+		while (it1.hasNext()) {
+			String projeto1 = it1.next();
+			it2 = mapProjetosMetricas.keySet().iterator();
+			ArrayList<ClassMetricResult> metricasProjeto1 = mapProjetosMetricas.get(projeto1);
+			ArrayList<ClassMetricResult> metricasProjeto2;
+			while (it2.hasNext()) {
+				String projeto2 = it2.next();
+				metricasProjeto2 = mapProjetosMetricas.get(projeto2);
+				Double similarity = calculate(metricasProjeto1, metricasProjeto2);
+				SimilarityResult result = new SimilarityResult();
+				result.setProject1(projeto1);
+				result.setProject2(projeto2);
+				result.setSimilarity(similarity);
+				listSimilarityResult.add(result);
+			}
+		}
+		return listSimilarityResult;
+	}
+
+	public Double calculate(ArrayList<ClassMetricResult> projeto1, ArrayList<ClassMetricResult> projeto2) {
 
 		DesignRoleTechnique designRoleTechinique = new DesignRoleTechnique();
 
-		float similaridade = (float) 0;
-
 		HashMap<String, Long> linhasDeCodigoPorDesignRoleProjeto1 = new HashMap<String, Long>();
-		Map<CharSequence, Integer> drProjeto1 = countDesignRoles(projeto1); 
-		Long totalLocProjeto1 = designRoleTechinique.obterTotalLinhasCodigoPorDesignRole(projeto1, linhasDeCodigoPorDesignRoleProjeto1);
-		
+		Long totalLocProjeto1 = designRoleTechinique.obterTotalLinhasCodigoPorDesignRole(projeto1,
+				linhasDeCodigoPorDesignRoleProjeto1);
+
 		if (linhasDeCodigoPorDesignRoleProjeto1.get("TEST") != null)
 			totalLocProjeto1 -= linhasDeCodigoPorDesignRoleProjeto1.remove("TEST");
 		if (linhasDeCodigoPorDesignRoleProjeto1.get("UNDEFINED") != null)
 			totalLocProjeto1 -= linhasDeCodigoPorDesignRoleProjeto1.remove("UNDEFINED");
-		
-		
+
 		HashMap<String, Long> linhasDeCodigoPorDesignRoleProjeto2 = new HashMap<String, Long>();
-		Map<CharSequence, Integer> drProjeto2 = countDesignRoles(projeto2); 
-		Long totalLocProjeto2 = designRoleTechinique.obterTotalLinhasCodigoPorDesignRole(projeto2, linhasDeCodigoPorDesignRoleProjeto2);
+
+		Long totalLocProjeto2 = designRoleTechinique.obterTotalLinhasCodigoPorDesignRole(projeto2,
+				linhasDeCodigoPorDesignRoleProjeto2);
 		if (linhasDeCodigoPorDesignRoleProjeto2.get("TEST") != null)
 			totalLocProjeto2 -= linhasDeCodigoPorDesignRoleProjeto2.remove("TEST");
 		if (linhasDeCodigoPorDesignRoleProjeto2.get("UNDEFINED") != null)
 			totalLocProjeto2 -= linhasDeCodigoPorDesignRoleProjeto2.remove("UNDEFINED");
 
-
-		for (CharSequence key : drProjeto1.keySet()) {
-			if (!key.equals("TEST") && !key.equals("UNDEFINED")) {
-				if (drProjeto2.containsKey(key)) {
-					if (linhasDeCodigoPorDesignRoleProjeto1.get(key) != null)
-						similaridade += (float) linhasDeCodigoPorDesignRoleProjeto1.get(key) / totalLocProjeto1;
-				} else {
-					if (linhasDeCodigoPorDesignRoleProjeto1.get(key) != null)
-						similaridade -= (float) linhasDeCodigoPorDesignRoleProjeto1.get(key) / totalLocProjeto1;
-				}
-			}
-		}
-
-		for (CharSequence key : drProjeto2.keySet()) {
-			if (!key.equals("TEST") && !key.equals("UNDEFINED")) {
-				if (drProjeto1.containsKey(key)) {
-					if (linhasDeCodigoPorDesignRoleProjeto2.get(key) != null)
-						similaridade += (float) linhasDeCodigoPorDesignRoleProjeto2.get(key) / totalLocProjeto2;
-				} else {
-					if (linhasDeCodigoPorDesignRoleProjeto2.get(key) != null)
-						similaridade -= (float) linhasDeCodigoPorDesignRoleProjeto2.get(key) / totalLocProjeto2;
-				}
-			}
-		}
-		
 		Map<CharSequence, Double> drProjetoPercentual1 = new HashMap<CharSequence, Double>();
 		Map<CharSequence, Double> drProjetoPercentual2 = new HashMap<CharSequence, Double>();
-		
+
 		for (CharSequence key : linhasDeCodigoPorDesignRoleProjeto1.keySet()) {
 			Double valor = ((double) linhasDeCodigoPorDesignRoleProjeto1.get(key) / totalLocProjeto1) * 100;
 			drProjetoPercentual1.put(key, valor);
@@ -73,46 +96,17 @@ public class SimilarityManager {
 			Double valor = ((double) linhasDeCodigoPorDesignRoleProjeto2.get(key) / totalLocProjeto2) * 100;
 			drProjetoPercentual2.put(key, valor);
 		}
-		
-		System.out.println("======>>>> Cosine Similarity = " + cosineSimilarity(drProjetoPercentual1, drProjetoPercentual2));
-		
-		similaridade = similaridade / 2;
-		BigDecimal bd = new BigDecimal(Float.toString(similaridade));
-		bd = bd.setScale(2, BigDecimal.ROUND_HALF_UP);
-		return bd.floatValue();
-	}
-	
-//	public float calculateCossineSimilarity(ArrayList<ClassMetricResult> projeto1, ArrayList<ClassMetricResult> projeto2) {
-//		
-//		Map<CharSequence, Integer> drProjeto1 = countDesignRoles(projeto1); 
-//		Map<CharSequence, Integer> drProjeto2 = countDesignRoles(projeto2); 
-//		
-//		
-//		Double similaridade  = cosineSimilarity(drProjeto1, drProjeto2);
-//		return similaridade.floatValue();
-//	}
-	
-	
 
-	private Map<CharSequence, Integer> countDesignRoles(ArrayList<ClassMetricResult> projeto) {
-		Map<CharSequence, Integer> drProjeto = new HashMap<CharSequence, Integer>();
-
-		for (ClassMetricResult ckNumber : projeto) {
-			Integer numero = drProjeto.get(ckNumber.getDesignRole());
-
-			if (numero != null)
-				drProjeto.put(ckNumber.getDesignRole(), ++numero);
-			else
-				drProjeto.put(ckNumber.getDesignRole(), 1);
-		}
-		return drProjeto;
+		return cosineSimilarity(drProjetoPercentual1, drProjetoPercentual2);
 	}
 
 	/**
 	 * Calculates the cosine similarity for two given vectors.
 	 *
-	 * @param leftVector left vector
-	 * @param rightVector right vector
+	 * @param leftVector
+	 *            left vector
+	 * @param rightVector
+	 *            right vector
 	 * @return cosine similarity between the two vectors
 	 */
 	private Double cosineSimilarity(final Map<CharSequence, Double> leftVector,
@@ -143,8 +137,11 @@ public class SimilarityManager {
 
 	/**
 	 * Returns a set with strings common to the two given maps.
-	 * @param leftVector left vector map
-	 * @param rightVector right vector map
+	 * 
+	 * @param leftVector
+	 *            left vector map
+	 * @param rightVector
+	 *            right vector map
 	 * @return common strings
 	 */
 	private Set<CharSequence> getIntersection(final Map<CharSequence, Double> leftVector,
@@ -158,9 +155,13 @@ public class SimilarityManager {
 	 * Computes the dot product of two vectors. It ignores remaining elements. It
 	 * means that if a vector is longer than other, then a smaller part of it will
 	 * be used to compute the dot product.
-	 * @param leftVector left vector
-	 * @param rightVector right vector
-	 * @param intersection  common elements
+	 * 
+	 * @param leftVector
+	 *            left vector
+	 * @param rightVector
+	 *            right vector
+	 * @param intersection
+	 *            common elements
 	 * @return the dot product
 	 */
 	private double dot(final Map<CharSequence, Double> leftVector, final Map<CharSequence, Double> rightVector,
---
+++ src/org/systemsimilarity/SimilarityResult.java
@@ -0,0 +1,27 @@
+package org.systemsimilarity;
+
+public class SimilarityResult {
+	private String project1;
+	private String project2;
+	private Double similarity;
+	public String getProject1() {
+		return project1;
+	}
+	public void setProject1(String project1) {
+		this.project1 = project1;
+	}
+	public String getProject2() {
+		return project2;
+	}
+	public void setProject2(String project2) {
+		this.project2 = project2;
+	}
+	public Double getSimilarity() {
+		return similarity;
+	}
+	public void setSimilarity(Double similarity) {
+		this.similarity = similarity;
+	}
+	
+
+}
